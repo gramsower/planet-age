7b0614ce11b3684b5db80c1bd430b1f3bf0cdbda
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-06 17:59:42 -0700

    add passing tests for Earth age and life expectency variables

--- __tests__/planetAge.tests.js
+++ __tests__/planetAge.tests.js
@@ -2,8 +2,9 @@ import Age from './../src/planetAge.js'
 
 describe('Age', () => {
   test("should return an age object containing the user's age and life expectency and empty values for ages and remaining expected time on other planets", () => {
-    let age = new Age(37);
+    let age = new Age(37, 80);
     expect(age.ageEarth).toEqual(37);
+    expect(age.earthLife).toEqual(80);
   });
 })
 
