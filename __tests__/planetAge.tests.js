6397a5dfe73d1202048e6cbbe9f393541501974e
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-06 23:55:52 -0700

    add text to calculate equivalent life expectancy on Mercury and receive a good fail

--- __tests__/planetAge.tests.js
+++ __tests__/planetAge.tests.js
@@ -2,7 +2,11 @@ import { createHtmlTagObject } from 'html-webpack-plugin';
 import Age from './../src/planetAge.js'
 
 describe('Age', () => {
-  let age = new Age(37, 80);
+  let age;
+
+  beforeEach(() => {
+    age = new Age(37, 80);
+  });
 
   test("should return an age object containing the user's age and life expectency and empty values for ages and remaining expected time on other planets", () => {
     expect(age.ageEarth).toEqual(37);
@@ -14,19 +18,26 @@ describe('Age', () => {
     expect(age.ageMerc).toEqual(154);
   });
 
-  test("should return the user's Venus age", () => {
-    age.venAge();
-    expect(age.ageVen).toEqual(59)
+  test("should calculate the user's equivalent life expectancy on Mercury, and return the remaining years of the user's life", () => {
+    age.mercAge();
+    expect(age.mercLife).toEqual(333);
+    expect(age.remainMercLife).toEqual(179);
   });
+    
 
-  test("should return the user's Mars age", () => {
-    age.marsAge();
-    expect(age.ageMars).toEqual(19)
-  });
+  // test("should return the user's Venus age", () => {
+  //   age.venAge();
+  //   expect(age.ageVen).toEqual(59)
+  // });
 
-  test("should return the user's Jupiter age", () => {
-    age.jupAge();
-    expect(age.ageJup).toEqual(3);
-  });
+  // test("should return the user's Mars age", () => {
+  //   age.marsAge();
+  //   expect(age.ageMars).toEqual(19)
+  // });
+
+  // test("should return the user's Jupiter age", () => {
+  //   age.jupAge();
+  //   expect(age.ageJup).toEqual(3);
+  // });
 
 });
--- src/planetAge.js
+++ src/planetAge.js
@@ -2,25 +2,42 @@ export default class Age {
   constructor(ageEarth, earthLife) {
     this.ageEarth = ageEarth;
     this.earthLife = earthLife;
+
     this.ageMerc = 0;
+    this.mercLife = 0;
+    this.remainMercLife = 0;
+
     this.ageVen = 0;
+    this.venLife = 0;
+    this.remainVenLife = 0;
+
     this.ageMars = 0;
+    this.marsLife = 0;
+    this.remainMarsLife = 0;
+
     this.ageJup = 0;
+    this.jupLife = 0;
+    this.remainJupLife = 0;
   };
 
   mercAge() {
     this.ageMerc = parseInt(this.ageEarth/.24);
+    this.mercLife = parseInt(this.earthLife/.24);
+    this.remainMercLife = parseInt(this.mercLife - this.ageMerc);
+
   };
 
-  venAge() {
-    this.ageVen = parseInt(this.ageEarth/.62);
-  };
+  // venAge() {
+  //   this.ageVen = parseInt(this.ageEarth/.62);
+  // };
 
-  marsAge() {
-   this.ageMars = parseInt(this.ageEarth/1.88);
-  };
+  // marsAge() {
+  //  this.ageMars = parseInt(this.ageEarth/1.88);
+  // };
 
-  jupAge() {
-    this.ageJup = parseInt(this.ageEarth/11.86);
-  };
+  // jupAge() {
+  //   this.ageJup = parseInt(this.ageEarth/11.86);
+  // };
+
+
 }
