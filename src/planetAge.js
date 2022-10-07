5f73c2f76071287588a7e9f00354d915e0eae397
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-06 17:40:45 -0700

    add first test and complete good fail

---
+++ src/planetAge.js
@@ -0,0 +1,14 @@
+export default class Age {
+  constructor(ageEarth, earthLife) {
+
+    // this.ageEarth = ageEarth;
+    // this.earthLife = earthLife;
+    // this.ageMerc = 0;
+    // this.ageVen = 0;
+    // this.ageMars = 0;
+    // this.ageJup = 0;
+  };
+};
+
+
+
