096f4bc88d64d1659b20d9468f312f3d7d1b1c69
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-08 08:35:47 -0700

    add tests to calculate life expectancy on Venus, Mars and Jupiter, and return remaining life expectancy for all three planets'

--- __tests__/planetAge.tests.js
+++ __tests__/planetAge.tests.js
@@ -8,7 +8,7 @@ describe('Age', () => {
     age = new Age(37, 80);
   });
 
-  test("should return an age object containing the user's age and life expectency and empty values for ages and remaining expected time on other planets", () => {
+  test("should return an age object containing the user's Earth age and life expectency, and empty values for ages and remaining expected time on other planets", () => {
     expect(age.ageEarth).toEqual(37);
     expect(age.earthLife).toEqual(80);
   });
@@ -18,11 +18,11 @@ describe('Age', () => {
     expect(age.ageMerc).toEqual(154);
   });
 
-  // test("should calculate the user's equivalent life expectancy on Mercury, and return the remaining years of the user's life in Mercury years", () => {
-  //   age.mercAge();
-  //   expect(age.mercLife).toEqual(333);
-  //   expect(age.remainMercLife).toEqual(179);
-  // });
+  test("should calculate the user's equivalent life expectancy on Mercury, and return the remaining years of the user's life in Mercury years", () => {
+     age.mercAge();
+     expect(age.mercLife).toEqual(333);
+     expect(age.remainMercLife).toEqual(179);
+   });
     
 
   test("should return the user's Venus age", () => {
@@ -31,20 +31,32 @@ describe('Age', () => {
 
   });
 
-  // test("should calculate the user's equivalent life expectancy on Venus, and return the remaining years of the user's life in Venus years", () => {
-  //   age.mercAge();
-  //   expect(age.mercLife).toEqual(333);
-  //   expect(age.remainMercLife).toEqual(179);
-  // });
+  test("should calculate the user's equivalent life expectancy on Venus, and return the remaining years of the user's life in Venus years", () => {
+     age.venAge();
+     expect(age.venLife).toEqual(129);
+     expect(age.remainVenLife).toEqual(70);
+   });
 
   test("should return the user's Mars age", () => {
     age.marsAge();
     expect(age.ageMars).toEqual(19)
   });
 
+  test("should calculate the user's equivalent life expectancy on Mars, and return the remaining years of the user's life in Mars years", () => {
+    age.marsAge();
+    expect(age.marsLife).toEqual(42);
+    expect(age.remainMarsLife).toEqual(23);
+  });
+
   test("should return the user's Jupiter age", () => {
     age.jupAge();
     expect(age.ageJup).toEqual(3);
+  });
+
+  test("should calculate the user's equivalent life expectancy on Jupiter, and return the remaining years of the user's life in Jupiter years", () => {
+    age.mercAge();
+    expect(age.jupLife).toEqual(6);
+    expect(age.remainJupLife).toEqual(3);
   });
 
 });
