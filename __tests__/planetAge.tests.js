6fd27068bc45c6dc51eab05e3b97e08c787cf7c4
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-15 12:57:08 -0700

    add test to return the amount by which a user has outlived their life expectency and receive good fail

--- __tests__/planetAge.tests.js
+++ __tests__/planetAge.tests.js
@@ -24,6 +24,11 @@ describe('Age', () => {
      expect(age.remainMercLife).toEqual(179);
    });
     
+  test("should return the amount by which a user has outlived their life expectency, if their age is greater than their expected life expectancy", () => {
+    let age = new Age(87, 80);
+    age.mercAge();
+    expect(age.overMercLife).toEqual(7);
+  });
 
   test("should return the user's Venus age", () => {
     age.venAge();
--- src/planetAge.js
+++ src/planetAge.js
@@ -6,6 +6,8 @@ export default class Age {
     this.ageMerc = 0;
     this.mercLife = 0;
     this.remainMercLife = 0;
+    this.overMercLife = 0;
+
 
     this.ageVen = 0;
     this.venLife = 0;
@@ -23,25 +25,28 @@ export default class Age {
   mercAge() {
     this.ageMerc = parseInt(this.ageEarth/.24);
     this.mercLife = parseInt(this.earthLife/.24);
-    this.remainMercLife = parseInt(this.mercLife - this.ageMerc);
-    // if (this.remainMercLife > 0)
+    //if (this.ageMarc >= this.mercLife) {
+    //  this.overMercLife = Math.abs(this.mercLife - this.ageMerc);
+    //} else {
+    //  this.remainMercLife = (this.mercLife - this.ageMerc)
+   // };
   };
 
   venAge() {
     this.ageVen = parseInt(this.ageEarth/.62);
     this.venLife = parseInt(this.earthLife/.62);
-    this.remainVenLife = parseInt(this.venLife - this.ageVen);
+    this.remainVenLife = Math.abs(this.venLife - this.ageVen);
   };
 
   marsAge() {
    this.ageMars = parseInt(this.ageEarth/1.88);
    this.marsLife = parseInt(this.earthLife/1.88);
-   this.remainMarsLife = parseInt(this.marsLife - this.ageMars);
+   this.remainMarsLife = Math.abs(this.marsLife - this.ageMars);
   };
 
   jupAge() {
     this.ageJup = parseInt(this.ageEarth/11.86);
     this.jupLife = parseInt(this.earthLife/11.86);
-    this.remainJupLife = parseInt(this.jupLife - this.ageJup);
+    this.remainJupLife = Math.abs(parseInt(this.jupLife - this.ageJup));
   };
 }
